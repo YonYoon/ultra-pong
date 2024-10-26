107511488feb8d78dab54513bbbaed487b258b95
YonYoon <zhzhzh1806@gmail.com>
2024-10-26 15:35:17 +0500

    implement dash

--- src/main.c
+++ src/main.c
@@ -1,3 +1,4 @@
+#include "stdio.h"
 #include "raylib.h"
 
 #include "resource_dir.h" // utility header for SearchAndSetResourceDir
@@ -12,12 +13,22 @@ typedef struct Ball
 	Vector2 speed;
 } Ball;
 
-Ball ball = {0};
-Rectangle paddle_left = {0};
-Rectangle paddle_right = {0};
+typedef struct Paddle
+{
+	Rectangle rect;
+	float speed;
+	float acceleration;
+	bool can_dash;
+	int dash_meter;
+} Paddle;
 
-void move_paddle_up(Rectangle *paddle);
-void move_paddle_down(Rectangle *paddle);
+Ball ball = {0};
+Paddle paddle_left = {0};
+Paddle paddle_right = {0};
+
+void move_paddle_up(Paddle *paddle);
+void move_paddle_down(Paddle *paddle);
+void dash_paddle(Paddle *paddle);
 
 int main()
 {
@@ -48,16 +59,27 @@ int main()
 	const float speed_multiplier = 1.1;
 
 	// Paddles setup
-	paddle_left.width = 20;
-	paddle_left.height = 100;
-	paddle_right.width = 20;
-	paddle_right.height = 100;
+	paddle_left.rect.width = 20;
+	paddle_left.rect.height = 100;
+	paddle_right.rect.width = 20;
+	paddle_right.rect.height = 100;
 
-	paddle_left.x = 50;
-	paddle_left.y = (SCREEN_HEIGHT / 2) - (paddle_left.height / 2);
+	paddle_left.rect.x = 50;
+	paddle_left.rect.y = (SCREEN_HEIGHT / 2) - (paddle_left.rect.height / 2);
 
-	paddle_right.x = SCREEN_WIDTH - 70;
-	paddle_right.y = (SCREEN_HEIGHT / 2) - (paddle_right.height / 2);
+	paddle_right.rect.x = SCREEN_WIDTH - 70;
+	paddle_right.rect.y = (SCREEN_HEIGHT / 2) - (paddle_right.rect.height / 2);
+
+	paddle_left.speed = 5.0;
+	paddle_right.speed = 5.0;
+	paddle_left.acceleration = 1.0;
+	paddle_right.acceleration = 1.0;
+
+	paddle_left.can_dash = false;
+	paddle_right.can_dash = false;
+
+	paddle_left.dash_meter = 0;
+	paddle_right.dash_meter = 0;
 
 	bool is_right_ultra = false;
 	bool is_left_ultra = false;
@@ -98,33 +120,38 @@ int main()
 		}
 
 		// Collision with left and right paddle
-		if (CheckCollisionCircleRec(ball.center, ball.radius, paddle_left))
+		if (CheckCollisionCircleRec(ball.center, ball.radius, paddle_left.rect))
 		{
 			ball.speed.x *= -speed_multiplier;
 			if (is_left_ultra)
-			{
 				PlaySound(fxUltra);
-			}
 			else
-			{
 				PlaySound(fxCollision);
-			}
 			is_left_ultra = true;
 			is_right_ultra = false;
+			paddle_left.dash_meter += 5;
 		}
-		if (CheckCollisionCircleRec(ball.center, ball.radius, paddle_right))
+		if (CheckCollisionCircleRec(ball.center, ball.radius, paddle_right.rect))
 		{
 			ball.speed.x *= -speed_multiplier;
 			if (is_right_ultra)
-			{
 				PlaySound(fxUltra);
-			}
 			else
-			{
 				PlaySound(fxCollision);
-			}
 			is_left_ultra = false;
 			is_right_ultra = true;
+			paddle_right.dash_meter += 5;
+		}
+
+		if (paddle_left.acceleration > 1.0)
+		{
+			paddle_left.can_dash = false;
+			paddle_left.acceleration -= 0.1;
+		}
+		if (paddle_right.acceleration > 1.0)
+		{
+			paddle_right.can_dash = false;
+			paddle_right.acceleration -= 0.1;
 		}
 
 		// Left paddle controls
@@ -133,21 +160,48 @@ int main()
 		if (IsKeyDown(KEY_W))
 			move_paddle_up(&paddle_left);
 
+		if (paddle_left.can_dash || paddle_left.dash_meter >= 100)
+		{
+			if (IsKeyDown(KEY_X))
+			{
+				dash_paddle(&paddle_left);
+				paddle_left.dash_meter = 0;
+			}
+		}
+
 		// Right paddle controls
-		if (IsKeyDown(KEY_DOWN))
+		if (IsKeyDown(KEY_J))
 			move_paddle_down(&paddle_right);
-		if (IsKeyDown(KEY_UP))
+		if (IsKeyDown(KEY_I))
 			move_paddle_up(&paddle_right);
+
+		if (paddle_right.can_dash || paddle_right.dash_meter >= 100)
+		{
+			if (IsKeyDown(KEY_M))
+			{
+				dash_paddle(&paddle_right);
+				paddle_right.dash_meter = 0;
+			}
+		}
 
 		BeginDrawing();
 
 		ClearBackground(WHITE);
 
-		DrawText(TextFormat("%i", left_player_score), 200, 100, 20, BLACK);
-		DrawText(TextFormat("%i", right_player_score), SCREEN_WIDTH - 200, 100, 20, BLACK);
+		DrawText(TextFormat("%i", left_player_score), 200, 100, 50, BLACK);
+		DrawText(TextFormat("%i", right_player_score), SCREEN_WIDTH - 200, 100, 50, BLACK);
 
-		DrawRectangleRec(paddle_left, BLACK);
-		DrawRectangleRec(paddle_right, BLACK);
+		if (paddle_left.dash_meter >= 100)
+		{
+			DrawText("DASH", 100, 100, 50, RED);
+		}
+		if (paddle_right.dash_meter >= 100)
+		{
+			DrawText("DASH", SCREEN_WIDTH - 150, 100, 50, RED);
+		}
+
+		DrawRectangleRec(paddle_left.rect, BLACK);
+		DrawRectangleRec(paddle_right.rect, BLACK);
 		DrawCircleV(ball.center, ball.radius, BLACK);
 
 		EndDrawing();
@@ -163,16 +217,21 @@ int main()
 	return 0;
 }
 
-void move_paddle_up(Rectangle *paddle)
+void move_paddle_up(Paddle *paddle)
 {
-	paddle->y -= 5;
-	if (paddle->y <= 0)
-		paddle->y = 0;
+	paddle->rect.y -= paddle->speed * paddle->acceleration;
+	if (paddle->rect.y <= 0)
+		paddle->rect.y = 0;
 }
 
-void move_paddle_down(Rectangle *paddle)
+void move_paddle_down(Paddle *paddle)
 {
-	paddle->y += 5;
-	if ((paddle->y + 100) >= SCREEN_HEIGHT)
-		paddle->y = SCREEN_HEIGHT - 100;
+	paddle->rect.y += paddle->speed * paddle->acceleration;
+	if ((paddle->rect.y + 100) >= SCREEN_HEIGHT)
+		paddle->rect.y = SCREEN_HEIGHT - 100;
+}
+
+void dash_paddle(Paddle *paddle)
+{
+	paddle->acceleration = 3.0;
 }
