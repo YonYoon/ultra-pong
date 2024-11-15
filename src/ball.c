cb4e0acd418c252e06f9b4bd1e49284c89829c04
YonYoon <zhzhzh1806@gmail.com>
2024-11-15 21:06:05 +0500

    change ball's init speed

--- src/ball.c
+++ src/ball.c
@@ -9,7 +9,7 @@ Ball ball_setup()
     ball.center.x = SCREEN_WIDTH / 2;
     ball.center.y = SCREEN_HEIGHT / 2;
     ball.radius = 15.0;
-    ball.speed.x = 3.0;
+    ball.speed.x = 4.0;
     ball.speed.y = 3.0;
     return ball;
 }
